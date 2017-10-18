4cf5fa7faaf3b1a390f0243e3cb7b48e377efbab
Christy Dennison <christy@mit.edu>
2017-10-18 19:12:29 +0000

    5cii works

--- quasars.py
+++ quasars.py
@@ -99,33 +99,58 @@ def split(full, wavelengths):
     ###############
     return left, right
 
-def dist(wavelength_indices, a, b):
+def dist(a, b):
     dist = 0
-    
-    for w in wavelength_indices:
+
+    for i in range(len(a)):
         dist += (a[i] - b[i])**2
 
     return dist
 
-def get_r_indices(wavelengths):
-    return range(150,450)
-
-def get_l_indices(wavelengths):
-    return range(0,150)
-
-def calc_all_dists(spectra, wavelength_indices):
-    dists = {}
+def calc_dists(spectra_train, spectra_test):
     m = spectra.shape[0]
+    dists = np.zeros((m,m))
     for i in range(m):
-        dists[i] = {}
         a = spectra[i]
         for j in range(m):
             b = spectra[j]
-            dists[i,j] = dist(wavelength_indices, a, b)
+            dists[i,j] = dist(a, b)
     return dists
 
-def h(index, dists):
-    return 
+def k_neighbors(index, dists, k):
+    return np.argsort(dists[index])[:k]
+
+def get_h(index, dists):
+    return np.max(dists[index])
+
+def ker(t):
+    return np.max(t-1, 0)
+
+def fhat_left(smoothed_spectra, test_right):
+    rights = smoothed_spectra[:, 150:450]
+    lefts = smoothed_spectra[:, 0:50]
+    r_dists = calc_dists(rights)
+    m = smoothed_spectra.shape[0]
+    n = smoothed_spectra.shape[1]
+    lefthats = np.zeros((m,50))
+    errors = np.zeros(m)
+    # go through all training examples
+    for i in range(m):
+        # for each neighbor
+        top_f_left = 0
+        bottom_f_left = 0
+        h = get_h(i, r_dists)
+        for n_index in k_neighbors(i, r_dists, 3):
+            nl = lefts[n_index]
+            d = r_dists[i, n_index]
+            k_score = ker(d/h)
+            top_f_left += k_score*nl
+            bottom_f_left += k_score
+        fhat_i = top_f_left/bottom_f_left
+        lefthats[i] = fhat_i
+        errors[i] = dist(lefts[i], fhat_i)
+    mean_error = np.mean(errors)
+    return lefthats, mean_error
 
 def func_reg(left_train, right_train, right_test):
     m, n = left_train.shape
